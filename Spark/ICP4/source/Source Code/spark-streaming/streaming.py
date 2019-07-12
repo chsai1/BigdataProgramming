a6b53fa3c50b75fa57bcc21cfebd51e522f246b7
saidivyachennupati <42659823+saidivyachennupati@users.noreply.github.com>
2019-07-11 20:59:10 -0500

    hjj

--- Spark/ICP4/source/Source Code/spark-streaming/streaming.py
+++ Spark/ICP4/source/Source Code/spark-streaming/streaming.py
@@ -1,8 +1,8 @@
 import sys
 import os
 
-os.environ["SPARK_HOME"] = "D:\\spark-2.3.1-bin-hadoop2.7\\"
-os.environ["HADOOP_HOME"]="D:\\Drive\\winutils"
+os.environ["SPARK_HOME"] = "C:/Users/saidivya/Desktop/spark-2.4.3-bin-hadoop2.7"
+os.environ["HADOOP_HOME"]="C:/winutils"
 
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
@@ -12,8 +12,6 @@ This is use for create streaming of text from txt files that creating dynamicall
 from files.py code. This spark streaming will execute in each 3 seconds and It'll
 show number of words count from each files dynamically
 """
-
-
 def main():
     sc = SparkContext(appName="PysparkStreaming")
     ssc = StreamingContext(sc, 3)   #Streaming will execute in each 3 seconds
@@ -24,7 +22,6 @@ def main():
     counts.pprint()
     ssc.start()
     ssc.awaitTermination()
-
 
 if __name__ == "__main__":
     main()
